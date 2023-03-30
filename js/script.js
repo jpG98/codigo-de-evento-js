df4d2015824528465a8da0a87056d5bb2167e37b
jp <105756115+jpG98@users.noreply.github.com>
2023-03-30 10:05:40 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function over(){
+    document.body.style.backgroundColor = '#000000'
+}
+
+function out(){
+    document.body.style.backgroundColor='#ffffff'
+}
+
+function blclick(){
+    document.body.style.backgroundColor='#b803ff';
+}
+
+function click(){
+    document.body.style.backgroundColor='#0bfce8';
+}
+
+function minhaFuncao(){
+    document.getElementById("texto").innerHTML = "Você clicou na caixa de texto!"
+}
